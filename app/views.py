228c92a57efe5b1778f6c0cb061a500a3e2ff19e
Medved <eevalushionip@gmail.com>
2024-09-26 18:44:12 +0600

    category setings

--- app/views.py
+++ app/views.py
@@ -10,10 +10,10 @@ def home_site(reqeust):
     return render(request=reqeust, template_name='app/home.html', context={"car":cars,"categories":category})
 
 def category_site(reqeust, pk):
-    category = Category.objects.filter(id=pk)
+    cars = Car.objects.filter(category_it=pk)
     category = Category.objects.all()
 
-    return render(request=reqeust, template_name='app/category.html', context={"category":category})
+    return render(request=reqeust, template_name='app/category.html', context={"categories":category, "car":cars})
 
 def car_site(reqeust, pk):
     cars = Car.objects.get(id=pk)
