f1e90981e6c4f31eb48a2dde822ced7b23c48380
Medved <eevalushionip@gmail.com>
2024-10-02 12:38:55 +0600

    design update

--- app/views.py
+++ app/views.py
@@ -7,13 +7,13 @@ def home_site(reqeust):
     cars = Car.objects.all()
     category = Category.objects.all()
 
-    return render(request=reqeust, template_name='app/home.html', context={"car":cars,"categories":category})
+    return render(request=reqeust, template_name='app/home.html', context={"cars":cars,"categories":category})
 
 def category_site(reqeust, pk):
     cars = Car.objects.filter(category_it=pk)
     category = Category.objects.all()
 
-    return render(request=reqeust, template_name='app/category.html', context={"categories":category, "car":cars})
+    return render(request=reqeust, template_name='app/category.html', context={"categories":category, "cars":cars})
 
 def car_site(reqeust, pk):
     cars = Car.objects.get(id=pk)
